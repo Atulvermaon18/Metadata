6c76cd6cf9def324e73361db1c49f37d227963fc
Atul Kumar A <atul.a@tvsnext.io>
2018-08-03 15:07:16 +0530

    dir rename

--- Incite/dataAccesLayer/migrations/insertviewDetail.sql
+++
@@ -1,3 +0,0 @@
-
-INSERT INTO __fl_MetaDataDetail
-SELECT'ATUL','Thinkpad',CURRENT_TIMESTAMP, 'thinkpad', CURRENT_TIMESTAMP, 0, 'IEG00001', '','1','ACCOUNT_MASTER_SEARCH','S','BODY','text','label_sample','element_view_sample','element_data_sample','element_len_sample','data_size_sample','1','','','','','','','','','','','','','','';
